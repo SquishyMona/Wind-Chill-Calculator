c5c32b79ad5a7d153a0c63b46d6b3764be7121db
SquishyMona <kotonefemc@gmail.com>
2021-09-16 12:20:49 -0400

    added program description in a comment

--- Code.py
+++ Code.py
@@ -1,7 +1,9 @@
+# This program calculates the wind chill outside
+
 print("Please enter the temperature:")
 T = input()
 W = 20
 
 wind_chill = (35.74+0.6215*T)+(-35.75+0.4275*T)*(W**.16)
 
-print(wind_chill)
+print("The wind chill is "); print(wind_chill)
